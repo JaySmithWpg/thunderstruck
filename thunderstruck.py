f0e3517c4bd5a86c5011e5b9a78136207a10ff7e
Jay Smith <jayvsmith@gmail.com>
2015-10-05 00:04:34 -0500

    Convert lightningmaps.org json into geojson

---
+++ thunderstruck.py
@@ -0,0 +1,26 @@
+import json
+
+INPUT_FILE = '00.json'
+OUTPUT_FILE = 'geo00.json'
+
+def convert_to_geostrike(raw_strike):
+    return {"type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [raw_strike["lat"], raw_strike["lon"]]},
+            "properties": {
+                "name": "Strike",
+                "time": raw_strike["time"]}}
+
+geo_strikes = []
+with open(INPUT_FILE, 'rt') as input_file:
+    for strike_data in input_file:
+        raw_strike = json.loads(strike_data)
+        geo_strikes.append(convert_to_geostrike(raw_strike))
+
+geo_file = {"type": "FeatureCollection",
+            "features": geo_strikes}
+
+with open(OUTPUT_FILE, 'wt') as output_file:
+    output_file.write(json.dumps(geo_file))
+
